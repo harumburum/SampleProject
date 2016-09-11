3be7c1b21d81ca21c56d886d8acc97c710c00b6f
Vladimir Litvinchik <litvinchik.vladimir@gmail.com>
2016-09-11 12:17:00 +0300

    Commit into master

--- SampleProject/Program.cs
+++ SampleProject/Program.cs
@@ -8,7 +8,7 @@ namespace SampleProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello from master.");
+            Console.WriteLine("Hello from master revision 1.");
         }
     }
 }
